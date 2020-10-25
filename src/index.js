d398e8ae0ca567139b07ca8301e401218f8a08a8
Anna Gudinskaya <ann.gudin@gmail.com>
2020-10-25 23:15:12 +0300

    init: done task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, compaineBracketsConfig) {
+  while (str) {
+    let k = 0
+    for (const i of compaineBracketsConfig) {
+      let compaineBrackets = i[0] + i[1]
+      if (str.includes(compaineBrackets)) {
+        str = str.replace(compaineBrackets, '')
+        k += 1
+      }
+    }
+    if (k === 0) {
+      break
+    }
+  }
+  return str == '' ? true : false
 }
